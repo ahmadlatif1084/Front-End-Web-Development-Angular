8767c47d67ccffe584931a25583b0e3954548641
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-12-19 16:53:07 +0500

    Assignment 1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Dish} from '../shared/dish';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+
 const DISHES:Dish[] =  [
   {
     id: '0',
