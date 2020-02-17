a99f61acba09a731c5725f56581185977ead9fdf
ahmadlatif1084 <ahmad.latif1084@gmail.com>
2020-02-17 05:05:47 -0700

    Complete Animations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
 import {Leader} from '../shared/leader';
 import {LeadersService} from '../services/leaders.service';
+import { flyInOut,expand } from '../animations/app.animation';
+import {baseURL} from '../shared/baseurl';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+    animations: [
+      flyInOut(),
+      expand()
+    ]
 })
 export class AboutComponent implements OnInit {
     leader:Leader[];
 
-  constructor(private leaderservice:LeadersService) { 
+
+  constructor(private leaderservice:LeadersService, @Inject('BaseURL') private BaseURL) { 
     
   }
 
